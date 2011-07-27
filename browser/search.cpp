a0ce3cc2dec1795660df60ee878f56c26f288356
yuan xinyu <yuan_xin_yu@hotmail.com>
2011-07-27 13:06:11 +0800

    explore context menu dir search

--- browser/search.cpp
+++ browser/search.cpp
@@ -8,6 +8,26 @@
 #include "webform.h"
 #include "../filesearch/common.h"
 
+#include <string>
+
+void searchAndReplace(std::wstring& value, std::wstring const& search,std::wstring const& replace)
+{
+    std::wstring::size_type  next;
+
+    for(next = value.find(search);        // Try and find the first match
+        next != std::wstring::npos;        // next is npos if nothing was found
+        next = value.find(search,next)    // search for the next match starting after
+                                          // the last match that was found.
+       )
+    {
+        // Inside the loop. So we found a match.
+        value.replace(next,search.length(),replace);   // Do the replacement.
+        next += replace.length();                      // Move to just after the replace
+                                                       // This is the point were we start
+                                                       // the next search from. 
+    }
+}
+
 HINSTANCE hInstance;
 HWND hMain; // Our main window
 HWND hwebf; // We declare this handle globally, just for convenience
@@ -62,6 +82,15 @@ static void exec_js(const wchar_t *function_name){
 		exec_js_str(buffer);
 }
 
+static void init_dir(){
+	wchar_t buffer[MAX_PATH*2];
+	wchar_t *cmdLine = GetCommandLineW();
+	std::wstring str(cmdLine), olds(L"\\"), news(L"\\\\");
+	searchAndReplace(str,olds,news);
+	wsprintf(buffer,L"try{init_dir('%s')}catch(e){}",str.c_str());
+	exec_js_str(buffer);
+}
+
 
 LRESULT CALLBACK PlainWndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 	switch (msg) {
@@ -84,8 +113,7 @@ LRESULT CALLBACK PlainWndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 		int id = LOWORD(wParam), code = HIWORD(wParam);
 		if (id == 103 && code == WEBFN_LOADED) {
 			loaded = true;
-			//show_title();
-			//exec_js();
+			init_dir();
 		} else if (id == 103 && code == WEBFN_CLICKED) {
 			const TCHAR *url = WebformLastClick(hwebf);
 			WebformGo(hwebf, url);
@@ -131,7 +159,7 @@ void TranslateAccKey(LPMSG lpMsg) {
 	}
 }
 
-int WINAPI WinMain(HINSTANCE h,HINSTANCE,LPSTR,int){
+int WINAPI WinMain(HINSTANCE h,HINSTANCE hPrevInstance,LPSTR s,int nCmdShow){
 	setPWD(NULL);
 	hInstance=h;
 	OleInitialize(0);
