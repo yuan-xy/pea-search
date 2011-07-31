11625f92912b502434a6b60ba48534764e5eebb9
yuan xinyu <yuan_xin_yu@hotmail.com>
2011-07-31 11:02:12 +0800

    misc

--- build.bat
+++ build.bat
@@ -2,6 +2,7 @@
 if "%1" == "clean" goto do_clean
 if "%1" == "plugin" goto do_build_plugin
 if "%1" == "gigaso" goto do_build
+if "%1" == "gigaso_install" goto do_build
 
 :do_build
 	cd Release
@@ -9,12 +10,14 @@ if "%1" == "gigaso" goto do_build
 	nmake
 	cd ..
 	if "%1" == "gigaso" goto end	
+	if "%1" == "gigaso_install" goto do_install	
 :do_build_plugin
 	cd ..\firebreath-1.4\Release
 	cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release ..
 	nmake
 	cd ..\..\gigaso
 	if "%1" == "plugin" goto end	
+:do_install
 	cd Release
 	cpack
 	cd ..
--- console-example/win_service_ctrl.c
+++ console-example/win_service_ctrl.c
@@ -8,14 +8,14 @@ int main(int argc, char *argv[]){
 		printf("usage: %s [create path_to_exe] | delete | start",argv[0]);
 	}else if(argc==2){
 		if(strcmp("delete",argv[1])==0){
-			SC_HANDLE hSc = OpenService(hScm, "gigaso", DELETE);
+			SC_HANDLE hSc = OpenServiceA(hScm, "gigaso", DELETE);
 			if (hSc == NULL) return 3;
 			if (!DeleteService (hSc)) return 3;
 			CloseServiceHandle (hSc);
 			return 0;
 		}
 		if(strcmp("start",argv[1])==0){
-			SC_HANDLE hSc = OpenService(hScm,  "gigaso", SERVICE_ALL_ACCESS);
+			SC_HANDLE hSc = OpenServiceA(hScm,  "gigaso", SERVICE_ALL_ACCESS);
 			if (hSc == NULL) return 4;
 			if (!StartService (hSc, 0, NULL)) return 4;
 			CloseServiceHandle (hSc);
@@ -24,7 +24,7 @@ int main(int argc, char *argv[]){
 		printf("usage: %s [create path_to_exe] | delete | start",argv[0]);
 	}else{
 		if(strcmp("create",argv[1])==0){
-			SC_HANDLE hSc = CreateService (hScm, "gigaso", "Gigaso File Searcher",
+			SC_HANDLE hSc = CreateServiceA (hScm, "gigaso", "Gigaso File Searcher",
 				SERVICE_ALL_ACCESS, SERVICE_WIN32_OWN_PROCESS,
 				SERVICE_AUTO_START, SERVICE_ERROR_NORMAL,
 				argv[2], NULL, NULL, NULL, NULL, NULL);
