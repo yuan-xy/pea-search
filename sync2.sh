d016ff173dfd20308384ac771387ea620eb174d9
yuanxy <yuan_xin_yu@hotmail.com>
2011-10-20 11:00:24 +0800

    misc

--- sync2.sh
+++ sync2.sh
@@ -1,1 +1,3 @@
+chmod +x gserver/public/dispatch.fcgi
+zip gserver/public/PeaSrch.zip ReleaseWin/PeaSearch-1.0.19-x86.exe
 rsync -avz --exclude '*.bak' --exclude '*.exe' --exclude '*.dmp' gserver dooo@www.1dooo.com:/home/dooo  
