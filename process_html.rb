2ddf99243ecd48b7b28b537f415f65fd997b9a45
yuanxy <yuan_xin_yu@hotmail.com>
2011-11-07 17:49:11 +0800

    misc

--- process_html.rb
+++ process_html.rb
@@ -15,8 +15,14 @@ begin
   else
     os='linux'
   end
+  app_ico='images/app.icns'
+  media_ico='images/media.icns'
+  video_ico='images/media.icns'
 rescue
   os='win'
+  app_ico='icons/exe.ico'
+  media_ico='icons/media.ico'
+  video_ico='icons/video.ico'
 end
 
 File.open("#{web}/search2.htm","w") do |fo|
