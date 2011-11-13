6b363e73ec0309e7358b8d85aa5908ff12b49ac9
yuanxy <yuan_xin_yu@hotmail.com>
2011-11-13 12:05:00 +0800

    查看下载记录

--- gserver/app/controllers/dcounts_controller.rb
+++ gserver/app/controllers/dcounts_controller.rb
@@ -1,8 +1,18 @@
 class DcountsController < ApplicationController
-  # GET /dcounts
-  # GET /dcounts.xml
+
+
   def index
-    render :text => "#{Dcount.count+100}"
+    respond_to do |format|
+      format.html { 
+        if session[:yuan]=="xinyu" || params[:yuan]=="xinyu"
+          @dcounts = Dcount.paginate(:order => genOrder, :page => params[:page], :per_page =>20)
+          render
+        else
+          redirect_to "/404.html"
+        end
+        }
+      format.js  { render :text => "#{Dcount.count+100}" }
+    end
   end
 
   # POST /dcounts
@@ -14,4 +24,11 @@ class DcountsController < ApplicationController
     render :text => "#{Dcount.count}"
   end
 
+   private
+
+
+  def genOrder
+    s =" id desc"
+  end
+  
 end
