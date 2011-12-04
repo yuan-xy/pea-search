617bfe8c4e1a9f1af135b0344b25b4eb1805b178
yuanxy <yuan_xin_yu@hotmail.com>
2011-12-04 21:02:20 +0800

    cmake build file for gtk-webkit

---
+++ browserGtk/js.h
@@ -0,0 +1,12 @@
+#include <gtk/gtk.h>
+#include <stdlib.h>
+#include <webkit/webkit.h>
+#include <JavaScriptCore/JavaScript.h>
+
+
+extern void register_js(
+            WebKitWebView  *wv,
+            WebKitWebFrame *wf,
+            gpointer        ctx,
+            gpointer        arg3,
+			gpointer        user_data);
